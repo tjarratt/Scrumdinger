2e1387c49e990abc841fd93461d08619909f6e17
Tim Jarratt <tjarratt@gmail.com>
2021-02-15 12:52:23 +0100

    Initial commit -- empty project

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Tim Jarratt on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
