93fccfb41df1c5c75987f72f0df0efe7b92230ca
Tim Jarratt <tjarratt@gmail.com>
2021-02-16 15:14:21 +0100

    Add an edit view

--- ScrumDinger/DetailView.swift
+++ ScrumDinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
 	let scrum: DailyScrum
+	@State private var isPresented = false
 
     var body: some View {
 		List {
@@ -45,6 +46,20 @@ struct DetailView: View {
 		}
 		.listStyle(InsetGroupedListStyle())
 		.navigationTitle(scrum.title)
+		.navigationBarItems(trailing: Button("Edit") {
+			isPresented = true
+		})
+		.fullScreenCover(isPresented: $isPresented, content: {
+			NavigationView {
+				EditView()
+					.navigationTitle(scrum.title)
+					.navigationBarItems(leading: Button("Cancel") {
+						isPresented = false
+					}, trailing: Button("Done") {
+						isPresented = false
+					})
+			}
+		})
     }
 }
 
--- ScrumDinger/Models/DailyScrum.swift
+++ ScrumDinger/Models/DailyScrum.swift
@@ -24,14 +24,36 @@ struct DailyScrum: Identifiable {
 }
 
 extension DailyScrum {
+	struct Data {
+		var title = ""
+		var attendees: [String] = []
+		var lengthInMinutes: Double = 0.0
+		var color = Color.random
+	}
+
+	var data: Data {
+		return Data(title: title,
+					attendees: attendees,
+					lengthInMinutes: Double(lengthInMinutes),
+					color: color)
+	}
+}
+
+extension DailyScrum {
 	static var fixtures: [DailyScrum] {
 		[
-			DailyScrum(title: "Design Hangout", attendees: ["Jessica", "Abdou", "Anna"],
-					   lengthInMinutes: 10, color: Color("Design")),
-			DailyScrum(title: "Product Management", attendees: ["Michelle", "Gon", "Gabrielle"],
-					   lengthInMinutes: 5, color: Color("Product Management")),
-			DailyScrum(title: "Engineering", attendees: ["Daily", "Karine", "Sandrine", "Flo"],
-					   lengthInMinutes: 15, color: Color("Engineering"))
+			DailyScrum(title: "Design Hangout",
+					   attendees: ["Jessica", "Abdou", "Anna"],
+					   lengthInMinutes: 10,
+					   color: Color("Design")),
+			DailyScrum(title: "Product Management",
+					   attendees: ["Michelle", "Gon", "Gabrielle"],
+					   lengthInMinutes: 5,
+					   color: Color("Product Management")),
+			DailyScrum(title: "Engineering",
+					   attendees: ["Daily", "Karine", "Sandrine", "Flo"],
+					   lengthInMinutes: 15,
+					   color: Color("Engineering"))
 		]
 	}
 }
